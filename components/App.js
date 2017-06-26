f8c805507d09014e97113823a96eb0ad5e1092e4
Pavlo Bazyliuk <pavlo.bazyliuk@eleks.com>
2017-06-26 13:39:58 +0300

    added initial project files

---
+++ components/App.js
@@ -0,0 +1,18 @@
+// Libs
+import React, { Component } from 'react';
+
+
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="scoreboard">
+        <h1>hello it's me - a react starter app file</h1>
+      </div>
+    );
+  }
+};
+
+
+export default App;
---
+++ index.js
@@ -0,0 +1,11 @@
+// Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import App from './components/App';
+
+// CSS
+import './css/style.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
